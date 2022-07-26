1e4d9a15468f99ae7c6bd52529986337bc16f70f
TulioLeitao <tulioleitao@gmail.com>
2022-07-25 22:54:01 -0300

    correção no exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,13 +1,16 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
 
-console.log (minhaString.length);
+const tamanho=  minhaString.length;
 
-console.log (minhaString.trim());
+const aparada= minhaString.trim();
+const tamanhoAparado= aparada.length;
 
-const minhaStringAparada = minhaString.trim()
-console.log (minhaStringAparada.length);
 /*Aqui criei uma nova variavel para armazenar a minhaString depois do trim,
 sem essa variavel nova, o length retorna 58 carcateres antes E depois do trim.*/
 
-console.log (minhaStringAparada.replace ("________", "sticioso"));
+console.log (`a quantidade de caracteres antes de fazer o "trim"é de ${tamanho},
+a quantida de caracteres apos o "trim" é de ${tamanhoAparado}`)
+
+
+console.log (aparada.replace ("________", "sticioso"));
 
